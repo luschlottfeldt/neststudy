88b9288e9e3744b8d6a6e4c368a40dce4e1da962
Lucas - Oficial <lucas-oficial@MacBook-Pro-de-Lucas.local>
2022-05-04 11:37:16 -0300

    first commit

---
+++ src/courses/entities/course.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Tag } from './tag.entity';
+
+@Entity('courses')
+export class Course {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @JoinTable()
+  @ManyToMany(() => Tag, (tag: Tag) => tag.courses, { cascade: true })
+  tags: string[];
+}
